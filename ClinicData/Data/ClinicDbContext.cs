d025d0319227c3a99e8fc82bf2c60979f1ae8656
Prabhu.Udagatti <Prabhu.Udagatti@valtech.com>
2022-11-07 14:46:13 +0530

    Added

---
+++ ClinicData/Data/ClinicDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ClinicEntity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ClinicData.Data
+{
+    public class ClinicDbContext:DbContext
+    {
+        public ClinicDbContext()
+        {
+
+        }
+        public ClinicDbContext(DbContextOptions<ClinicDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Patient> patients { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer("Data Source=VDC01LTC2156;Initial Catalog=ClinicMngt; Integrated Security=true;");
+        }
+    }
+}
